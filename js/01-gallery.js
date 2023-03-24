4daf17d7ec1a14757fc389d65575c6f9b4eda909
GrabDmitro <d_grab2001@ukr.net>
2023-03-24 14:42:23 +0200

    fin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = galleryItems.map((image) => {
+  const fragment = document.createElement("li");
+  fragment.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = image.original;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = image.preview;
+  img.alt = image.description;
+  img.dataset.source = image.original;
+
+  fragment.appendChild(link);
+  link.appendChild(img);
+  return fragment;
+});
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName != "IMG") {
+    return;
+  }
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="${e.target.dataset.source}">
+	`
+    )
+    .show();
+});
+galleryRef.append(...list);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = galleryItems.map((image) => {
+  const fragment = document.createElement("li");
+  fragment.classList.add("gallery__item");
 
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = image.original;
+
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = image.preview;
+  img.alt = image.description;
+  // img.dataset.source = image.original;
+
+  fragment.appendChild(link);
+  link.appendChild(img);
+  return fragment;
+});
+const galleryRef = document.querySelector(".gallery");
 console.log(galleryItems);
+galleryRef.append(...list);
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionDelay: 250,
+  captionsData: "alt",
+});
